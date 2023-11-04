6edb64396a7b72003b0a12b9bbc3e500d8e26552
nnhao2003 <nhathaodev@gmail.com>
2023-11-04 15:26:19 +0700

    fixbugs: Find trans by id

--- src/services/payment.services.ts
+++ src/services/payment.services.ts
@@ -343,8 +343,7 @@ class PaymentServices {
       .leftJoinAndSelect('transaction.subscription', 'subscription')
       .leftJoinAndSelect('transaction.user', 'user')
       .where('transaction.is_active = true')
-      .where('transaction.id = :id', { id: id })
-      .orWhere('app_trans_id = :app_trans_id', { app_trans_id: id })
+      .andWhere('app_trans_id = :app_trans_id', { app_trans_id: id })
       .getOne();
     if (!res) {
       throw new AppError('Transaction not found', 404);
