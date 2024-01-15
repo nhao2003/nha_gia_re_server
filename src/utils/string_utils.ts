c8815f897e6493b72654d1b1ed0aaef92d05ba67
nhao2003 <nhathaodev@gmail.com>
2024-01-16 04:38:05 +0700

    Add removeVietnameseTones() method and update toSearchString() method in StringUtils class

--- src/utils/string_utils.ts
+++ src/utils/string_utils.ts
@@ -1,9 +1,20 @@
 class StringUtils {
-  // Unaccent string
+  // Bỏ dấu tiếng Việt
+  public static removeVietnameseTones(str: string): string {
+    str = str.toLowerCase();
+    const from = 'àáãảạăằắẵẳặâầấẫẩậèéẽẻẹêềếễểệđìíĩỉịòóõỏọôồốỗổộơờớỡởợùúũủụưừứữửựỳýỹỷỵ';
+    const to = 'aaaaaaaaaaaaaaaaaeeeeeeeeeeediiiiiooooooooooooooooouuuuuuuuuuuyyyyy';
+    for (let i = 0, l = from.length; i < l; i++) {
+      str = str.replace(RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+    return str;
+  }
+
   public static toSearchString(search: string): string {
-    search = search
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
+    // URL decode
+    search = decodeURIComponent(search);
+    console.log(search);
+    search = StringUtils.removeVietnameseTones(search)
       .replace(/[^a-zA-Z0-9\s]/g, '')
       .replace(/\s+/g, ' ')
       .trim();
